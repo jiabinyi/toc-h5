48763f4f943b8cb5e4f2999342d241ecc6597158
yinxingxian.sz <yinxingxian.sz@mopon.cn>
2022-08-08 16:58:40 +0800

    fix(或主题参数): 获取主题参数，切换主题

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
 import useTheme from '@/utils/hooks/useTheme'
 const theme = computed(() => {
   const route = useRoute()
-  return route?.query.value ?? 'blue'
+  return route?.query?.theme ?? 'blue'
 })
 const { setBodyClassName } = useTheme()
 setBodyClassName(theme.value ?? 'blue')
