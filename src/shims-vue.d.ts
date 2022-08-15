3def1f3b83052974bbdc2413d5d1ac2d8411cb29
yinxingxian.sz <yinxingxian.sz@mopon.cn>
2022-08-15 13:47:27 +0800

    fix(eslint.js): eslint校验优化

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -10,3 +10,19 @@ declare module 'qs' {
     [key: string]: any
   }
 }
+export interface UseVModelOptions<T> {
+  passive?: boolean
+  eventName?: string
+  deep?: boolean
+  defaultValue?: T
+}
+export declare function useVModel<
+  P extends object,
+  K extends keyof P,
+  Name extends string
+>(
+  props: P,
+  key?: K,
+  emit?: (name: Name, ...args: any[]) => void,
+  options?: UseVModelOptions<P[K]>
+): Ref<UnwrapRef<P[K]>> | WritableComputedRef<P[K]>
