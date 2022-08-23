e18407a63391bf99f44b79d1726a6d4aed7519f2
jiabinyi <958214829@qq.com>
2022-08-23 16:00:30 +0800

    fix:  [4422] 【幸运大转盘】【小程序】【邀请好友助力】活动结束，扫描二维码没有3s跳转小程序首页

--- src/views/marquee/components/dialogTipActivityFinish/index.vue
+++ src/views/marquee/components/dialogTipActivityFinish/index.vue
@@ -50,7 +50,7 @@ const { pause, resume } = useIntervalFn(
  * @returns {any}
  */
 const goToHome = async () => {
-  const url = 'pages/tabbar/index/index'
+  const url = '/pages/common/pages/webview?type=switchTab&target=index'
   const wx = await import('wechat-ts-sdk').then(module => module.default)
   wx.miniProgram.switchTab({ url }) // 跳到小程序原生页面
 }
